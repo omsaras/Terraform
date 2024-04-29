233d86f4598fac3c4d7892a9728beb264596ade9
rchauhan <chauhan-rahul@hotmail.com>
2024-04-30 09:13:08 +1000

    Creating VPC infrastructure in AWS

--- main.tf
+++ main.tf
@@ -9,12 +9,18 @@ terraform {
 
 # Configure the AWS Provider
 provider "aws" {
-  region = "us-east-1"
+  region = "ap-southeast-2"
+  # if mac/linux, profile is located at ~/.aws/config and ~/.aws/credentials
+  profile = "TerraformProfile"
 }
 
 # Create a VPC
 resource "aws_vpc" "sme_vpc" {
   cidr_block = "10.0.0.0/24"
+
+  tags = {
+    "Name" = "sme_vpc"
+  }
 }
 
 # create Subnets
@@ -22,62 +28,56 @@ resource "aws_subnet" "subnet_public_2a" {
     vpc_id = aws_vpc.sme_vpc.id
     availability_zone = "ap-southeast-2a"
     cidr_block = "10.0.0.0/26"
+    map_public_ip_on_launch = true
+
+    tags = {
+        "Name" = "subnet_public_2a"
+        "Environment" = "dev"
+    }
 }
 
 resource "aws_subnet" "subnet_private_2a" {
     vpc_id = aws_vpc.sme_vpc.id
     availability_zone = "ap-southeast-2a"
     cidr_block = "10.0.0.64/26"
+
+    tags = {
+        "Name" = "subnet_private_2a"
+        "Environment" = "dev"
+    }
 }
 
 resource "aws_subnet" "subnet_public_2b" {
     vpc_id = aws_vpc.sme_vpc.id
     availability_zone = "ap-southeast-2b"
     cidr_block = "10.0.0.128/26"
+    map_public_ip_on_launch = true
+
+    tags = {
+        "Name" = "subnet_public_2b"
+        "Environment" = "dev"
+    }
 }
 
 resource "aws_subnet" "subnet_private_2b" {
     vpc_id = aws_vpc.sme_vpc.id
     availability_zone = "ap-southeast-2b"
     cidr_block = "10.0.0.192/26"
-}
 
-resource "aws_security_group" "subnet_sg_public_1a" {
-  vpc_id = aws_subnet.subnet_public_1a.vpc_id
-
-  ingress {
-    cidr_blocks = [aws_subnet.subnet_public_1a.cidr_block]
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-  }
-}
-
-resource "aws_security_group" "subnet_sg_private_1a" {
-  vpc_id = aws_subnet.subnet_public_1a.vpc_id
-
-  ingress {
-    cidr_blocks = [aws_subnet.subnet_public_1a.cidr_block]
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-  }
-}
-
-resource "aws_security_group" "subnet_sg_private" {
-  vpc_id = aws_subnet.subnet_public_1a.vpc_id
-
-  ingress {
-    cidr_blocks = [aws_subnet.subnet_public_1a.cidr_block]
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-  }
+    tags = {
+        "Name" = "subnet_private_2b"
+        "Environment" = "dev"
+    }
 }
 
 # create Internet Gateway
 resource "aws_internet_gateway" "IGW" {
   vpc_id = aws_vpc.sme_vpc.id
+
+  tags = {
+    "Name" = "IGW"
+    "Environment" = "Dev"
+  }
 }
 
 # create route table for public subnets
@@ -86,16 +86,12 @@ resource "aws_route_table" "rtb_igw" {
   # request coming from any IP should redirect to Internet Gateway
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.IGW
-  }
-  # any request within VPC 
-  route {
-    cidr_block = "10.0.0.0/24"
-    gateway_id = "local"
+    gateway_id = aws_internet_gateway.IGW.id
   }
 
   tags = {
-    Name = "rtb_igw"
+    "Name" = "rtb_igw"
+    "Environment" = "Dev"
   }
 }
 
@@ -111,50 +107,73 @@ resource "aws_route_table_association" "rtb_association_igw_2b" {
 }
 
 resource "aws_route_table_association" "rtb_association_nat_2a" {
-    route_table_id = aws_route_table.rtb_igw.id
+    route_table_id = aws_route_table.rtb_nat.id
     subnet_id = aws_subnet.subnet_private_2a.id
 }
 
 resource "aws_route_table_association" "rtb_association_nat_2b" {
-    route_table_id = aws_route_table.rtb_igw.id
+    route_table_id = aws_route_table.rtb_nat.id
     subnet_id = aws_subnet.subnet_private_2b.id
 }
 # ----------------------end route table association--------------------#
 
-resource "aws_route_table_association" "a" {
-  subnet_id      = aws_subnet.foo.id
-  route_table_id = aws_route_table.bar.id
-}
-
 # create route table for private subnets
 resource "aws_route_table" "rtb_nat" {
   vpc_id = aws_vpc.sme_vpc.id
+
+  tags = {
+    "Name" = "rtb_nat"
+    "Environment" = "Dev"
+  }
 }
 
-# create route table association for private subnets
-resource "aws_route_table_association" "route_table_association_private" {
-    route_table_id = aws_route_table.private_route_table.id
+# create elastic IP to assign to NAT gateway 
+# for private subnet in 2a availability zone
+resource "aws_eip" "elastic_ip_nat_2a" {
+   domain = "vpc"
+
+   tags = {
+     "Name" = "elastic_ip_nat_2a"
+   }
 }
 
-# create route table association for public subnets
-resource "aws_route_table_association" "route_table_association_public" {
-    route_table_id = aws_route_table.public_route_table.id
-}
+# create elastic IP to assign to NAT gateway 
+# for private subnet in 2b availability zone
+resource "aws_eip" "elastic_ip_nat_2b" {
+   domain = "vpc"
 
-resource "aws_eip" "nat_elastic_ip" {
-   
+   tags = {
+     "Name" = "elastic_ip_nat_2b"
+   }
 }
 
 #create NAT in public subnet 
-resource "aws_nat_gateway" "nat" {
-  allocation_id = aws_eip.example.id
-  subnet_id     = aws_subnet.example.id
+resource "aws_nat_gateway" "nat_2a" {
+  allocation_id = aws_eip.elastic_ip_nat_2a.id
+  subnet_id     = aws_subnet.subnet_private_2a.id
 
   tags = {
-    Name = "gw NAT"
+    "Name" = "nat_2a"
+    "Environment" = "Dev"
   }
 
   # To ensure proper ordering, it is recommended to add an explicit dependency
   # on the Internet Gateway for the VPC.
-  depends_on = [aws_internet_gateway.example]
+  depends_on = [aws_internet_gateway.IGW]
+}
+
+
+#create NAT in public subnet 
+resource "aws_nat_gateway" "nat_2b" {
+  allocation_id = aws_eip.elastic_ip_nat_2b.id
+  subnet_id     = aws_subnet.subnet_private_2b.id
+
+  tags = {
+    "Name" = "nat_2b"
+    "Environment" = "Dev"
+  }
+
+  # To ensure proper ordering, it is recommended to add an explicit dependency
+  # on the Internet Gateway for the VPC.
+  depends_on = [aws_internet_gateway.IGW]
 }
